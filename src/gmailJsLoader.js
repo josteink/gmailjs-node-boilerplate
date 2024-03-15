7c7cc2ec45a6c66a36c5013f9197859ef418e477
Jostein Kjønigsen <jostein@kjonigsen.net>
2024-03-15 15:42:35 +0100

    Update boilerplate to work with GMail and trusted HTML.

--- src/gmailJsLoader.js
+++ src/gmailJsLoader.js
@@ -11,8 +11,16 @@
 // Let the "big" extension bundle load separately!
 
 const GmailFactory = require("gmail-js");
-const jQuery = require("jquery");
+import $ from "jquery";
+
+const trustedHTMLpolicy = trustedTypes.createPolicy("default", {
+    createHTML: (to_escape) => to_escape,
+});
+
+$.extend({
+    htmlPrefilter: trustedHTMLpolicy.createHTML // this is the actual function which jQuery needs
+});
 
 // don't mess up too bad if we have several gmail.js-based
 // extensions loaded at the same time!
-window._gmailjs = window._gmailjs || new GmailFactory.Gmail(jQuery);
+window._gmailjs = window._gmailjs || new GmailFactory.Gmail($);
